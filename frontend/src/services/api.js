d3830030da6114f731aba71fe7cb7004e98b7930
pablopriegu <pablopriegu@gmail.com>
2025-07-07 18:58:22 -0600

    Configure frontend for production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api'
+  baseURL: 'https://agua-escondida-backend.onrender.com/api'
 });
 
 // Interceptor para añadir el token a las cabeceras
